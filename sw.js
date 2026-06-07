eb6e96b575e1bde81a2b638c2a85a1a634fbd14e
perruolo <95003824+perruolo@users.noreply.github.com>
2026-06-07 02:32:01 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,30 @@
+const CACHE = 'viworkout-v1';
+const ASSETS = [
+  '/workout-app/',
+  '/workout-app/index.html',
+  '/workout-app/manifest.json',
+  '/workout-app/icon-192.png',
+  '/workout-app/icon-512.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request))
+  );
+});
